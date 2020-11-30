cb019037f6c4460f14cfe68b9e8d1fd8b5eefb9e
colazeus <mail@2wyp.com>
2020-11-30 13:45:43 +0800

    gzip5

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-import './plugins/element.js'
+//import './plugins/element.js'
 import router from './router'
 import store from './store'
 import api from './api/api.js'
-import VCharts from 'v-charts'
+//import VCharts from 'v-charts'
 import VueFab from 'vue-float-action-button'
 import IconSvg from './components/icon-component'
 import VideoCard from './components/video-component'
@@ -14,7 +14,7 @@ Vue.config.productionTip = false
 Vue.component('icon-svg', IconSvg)
 Vue.component('video-card',VideoCard)
 Vue.component('video-list',VideoList)
-Vue.use(VCharts)
+//Vue.use(VCharts)
 Vue.use(VueFab)
 
 Vue.prototype.$api = api;
