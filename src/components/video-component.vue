a27a49ba66808284cc781222b56a1f9f50d95b11
colazeus <mail@2wyp.com>
2020-12-09 01:20:28 +0800

    update

--- src/components/video-component.vue
+++ src/components/video-component.vue
@@ -5,12 +5,20 @@
     <span>{{title}}</span>
     <div class="time">{{create_time | formatData}} <span @click="clickUp">{{owner_name}}</span></div>
     <div class="view"> {{v_view | formatNum}}播放 · {{v_danmaku | formatNum}}弹幕</div>
-    <div class="param-list">
-      <span><icon-svg icon-class="bl-icon-zan" />{{v_like | formatNum}}</span>
-      <span><icon-svg icon-class="bl-icon-Bbi" />{{v_coin | formatNum}}</span>
-      <span><icon-svg icon-class="bl-icon-shoucang" />{{v_favorite | formatNum}}</span>
-      <span><icon-svg icon-class="bl-icon-fenxiang" />{{v_share | formatNum}}</span>
-    </div>
+    <el-row class="param-list">
+      <el-col :span="6">
+        <icon-svg icon-class="bl-icon-zan" />{{v_like | formatNum}}
+      </el-col>
+      <el-col :span="6">
+        <icon-svg icon-class="bl-icon-Bbi" />{{v_coin | formatNum}}
+      </el-col>
+      <el-col :span="6">
+        <icon-svg icon-class="bl-icon-shoucang" />{{v_favorite | formatNum}}
+      </el-col>
+      <el-col :span="6">
+        <icon-svg icon-class="bl-icon-fenxiang" />{{v_share | formatNum}}
+      </el-col>
+    </el-row>
   </div>
 </el-card>
 </template>
@@ -24,20 +32,19 @@ export default {
     }
   },
   props: {
-    title:String,
-    pic:String,
-    create_time:String,
-    owner_name:String,
-    v_like:String,
-    v_coin:String,
-    v_favorite:String,
-    v_share:String,
-    v_view:String,
-    v_danmaku:String,
+    title: String,
+    pic: String,
+    create_time: String,
+    owner_name: String,
+    v_like: String,
+    v_coin: String,
+    v_favorite: String,
+    v_share: String,
+    v_view: String,
+    v_danmaku: String,
   },
   methods: {
-    clickUp: function() {
-    }
+    clickUp: function() {}
   }
 }
 </script>
@@ -55,13 +62,13 @@ export default {
 }
 
 .el-card .time span {
-  margin-top:6px;
+  margin-top: 6px;
   color: #409EFF;
   cursor: pointer;
   padding-left: 8px;
 }
 
-.el-card .view{
+.el-card .view {
   color: #969696;
   font-size: 0.8em;
   margin-top: 8px;
@@ -73,9 +80,8 @@ export default {
   padding-top: 12px;
 }
 
-.param-list span {
-  margin-right:10px;
-  font-size:0.9em;
+.param-list .el-col {
+  font-size: 0.9em;
 }
 
 .param-list svg {
